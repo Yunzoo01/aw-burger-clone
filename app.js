fb31509f6c9e9ac80cc8f392e7b82d1fdffaea45
Yunzoo01 <joyunzoooo@gmail.com>
2024-09-23 23:30:46 -0700

    devide html footer and navbar

---
+++ app.js
@@ -0,0 +1,14 @@
+// Fetch and load the navbar.html into the main page
+fetch("./partials/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar-container").innerHTML = data;
+  })
+  .catch((error) => console.error("Error loading navbar:", error));
+
+fetch("./partials/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer-container").innerHTML = data;
+  })
+  .catch((error) => console.error("Error loading footer:", error));
