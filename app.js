05fdbad941a867fab4bfa86f8da6c59bc8f98b6c
Yunzoo01 <joyunzoooo@gmail.com>
2024-09-24 17:32:50 -0700

    setting the tailwind

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // Fetch and load the navbar.html into the main page
-fetch("./partials/navbar.html")
+fetch("../partials/navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar-container").innerHTML = data;
   })
   .catch((error) => console.error("Error loading navbar:", error));
 
-fetch("./partials/footer.html")
+fetch("../partials/footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("footer-container").innerHTML = data;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.html',     // pages 폴더 안의 모든 HTML 파일
+    './partials/**/*.html',  // partials 폴더 안의 모든 HTML 파일
+    './styles/**/*.css',     // styles 폴더 안의 모든 CSS 파일
+    './index.html',          // index.html 파일
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-primary': '#ff6218', // 사용자 정의 색상
+        
+      },
+      screens: {
+        'desktop': '850px', // 850px 이상일 때 적용될 데스크탑 브레이크포인트
+      }
+    },
+  },
+  plugins: [],
+}
